1b2569decce34c1e4db31bdb79582fbee7ba03ed
Eden Mengistu <eden.g.mengistu@gmail.com>
2025-04-22 15:36:04 -0500

    done with FamousQuotes

---
+++ FamousQuotes/src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class FamousQuotes {
+    public static void main(String[]args){
+       String[] quotes = new String[] {
+               "1.The purpose of our lives is to be happy - Dalai Lama",
+               "2.Love is the master key that opens the gates of happiness- Oliver Holmes",
+               "3.if you find someone you love in your life,then hang on to that love- Princess Diana",
+               "4. I am always ready to learn although i do not always like being taught- Winston Churchill",
+               "5. Live as if you were to die tomorrow.learn as if you were to live forever - Mahatma Gandhi",
+               "6.Develop a passion for learning.if you do, you will never cease to grow- Anthony Angelo ",
+               "7. You don't understand anything until you learn it more than one way - Marvin Minsky",
+               "8. We are not prefect, forgive others as you would want to be forgiven",
+               "9. Always forgive your enemies",
+               "10. be happy",
+       };
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("Choose a number to get a quote:");
+       int index = input.nextInt();
+       //Display quote
+
+
+
+       }
+    }
+}
